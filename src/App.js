6fa1fc8357c50e83583c4e21fee7b81375fe07ab
moijesuisben <benji.apg@gmail.com>
2020-06-17 00:31:36 +0200

    teaser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import { createUseStyles } from "react-jss";
+import * as variables from "./variables";
+import Logo from "./components/Logo";
+import HALO from "vanta/dist/vanta.halo.min";
+import ReactAudioPlayer from "react-audio-player";
+
+const useStyles = createUseStyles({
+  landing_background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100vh",
+    width: "100vw",
+    // backgroundImage: "url(media/medium.jpg)",
+    // backgroundSize: "cover",
+  },
+  landing_content: {
+    height: "100vh",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  landing_text: {
+    textAlign: "center",
+    "& > *": {
+      color: variables.pumice,
+      textTransform: "uppercase",
+    },
+  },
+  landing_title: {
+    fontSize: variables.fs_xl,
+    fontWeight: variables.fw_light,
+    marginBottom: variables.space1,
+  },
+  landing_subtitle: {
+    fontSize: variables.fs_sm,
+    fontWeight: variables.fw_medium,
+    marginBottom: variables.space1,
+  },
+  landing_enter: {
+    fontStyle: variables.fs_sm,
+    fontWeight: variables.fw_bold,
+    textDecoration: "none",
+    "&:hover": {
+      color: variables.pumice,
+      "-webkit-text-stroke": "0px transparent",
+      textShadow: "0px 0px 10px rgba(203,204,203,1)",
+    },
+  },
+  landing_contact: {
+    fontSize: variables.fs_xs,
+    fontWeight: variables.fw_medium,
+    color: variables.pumice,
+    textTransform: "uppercase",
+    textDecoration: "none",
+    position: "absolute",
+    bottom: variables.space1,
+    right: variables.space1,
+    marginBottom: "calc(10px + (20 - 10) * ((100vw - 320px) / (1920 - 320)))",
+    "&:hover": {
+      color: variables.pumice,
+      "-webkit-text-stroke": "0px transparent",
+      textShadow: "0px 0px 10px rgba(203,204,203,1)",
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        HALO({
+          el: vantaRef.current,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <ReactAudioPlayer src="sound.mp3" autoPlay loop preload volume={0.5} />
+      <div className={classes.landing_background} ref={vantaRef}></div>
+      <div className={classes.landing_content}>
+        <div className={classes.landing_text}>
+          <h1 className={classes.landing_title}>benjamin arpage</h1>
+          <h2 className={classes.landing_subtitle}>
+            Creative Front-End Developer
+          </h2>
+        </div>
+        <Logo />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className={classes.landing_contact}
+          href="mailto:benjaminarpage@gmail.com"
         >
-          Learn React
+          Contact
         </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/variables.js
@@ -0,0 +1,30 @@
+
+/* FONT SIZE */
+export const fs_xs ='calc(12px + (15 - 12) * ((100vw - 320px) / (1920 - 320)))';
+export const fs_sm ='calc(15px + (30 - 15) * ((100vw - 320px) / (1920 - 320)))';
+export const fs_md ='calc(20px + (40 - 20) * ((100vw - 320px) / (1920 - 320)))';
+export const fs_lg ='calc(25px + (50 - 25) * ((100vw - 320px) / (1920 - 320)))';
+export const fs_xl ='calc(40px + (80 - 40) * ((100vw - 320px) / (1920 - 320)))';
+
+/* FONT WEIGHT */
+export const fw_thin = 100;
+export const fw_light = 300;
+export const fw_medium = 500;
+export const fw_bold = 700;
+export const fw_black = 900;
+
+
+/* COLOR */
+export const deepFire = '#020400';
+export const toryBlue = '#0B5AA2';
+export const pumice = '#CBCCCB';
+export const heavyMetal = '#393A38';
+export const white = '#f8f9fa';
+
+/* SPACING */
+
+export const space1 = 'calc(25px + (50 - 25) * ((100vw - 320px) / (1920 - 320)))';
+export const space2 = 'calc(50px + (100 - 50) * ((100vw - 320px) / (1920 - 320)))';
+export const space3 = 'calc(75px + (150 - 75) * ((100vw - 320px) / (1920 - 320)))';
+
+/*  */
