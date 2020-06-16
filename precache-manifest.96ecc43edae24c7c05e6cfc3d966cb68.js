3a9a6ea43a9194d88a6d8e920ee32320472c2a63
moijesuisben <benji.apg@gmail.com>
2020-06-17 00:49:18 +0200

    Updates

---
+++ precache-manifest.96ecc43edae24c7c05e6cfc3d966cb68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100a862e129efe5a21198d9c97eb1713",
+    "url": "/teaser/index.html"
+  },
+  {
+    "revision": "47ad5a2eee0e90080f3b",
+    "url": "/teaser/static/css/main.575e074e.chunk.css"
+  },
+  {
+    "revision": "6af45d88245117265b66",
+    "url": "/teaser/static/js/2.480cba71.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teaser/static/js/2.480cba71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ad5a2eee0e90080f3b",
+    "url": "/teaser/static/js/main.30bff647.chunk.js"
+  },
+  {
+    "revision": "0443e51c251bb23284f2",
+    "url": "/teaser/static/js/runtime-main.4f6f54ed.js"
+  }
+]);
